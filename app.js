f4457a06460e9de0af954ec66e0c06dad494582a
Olezhka-web <zcegthrkfcghj5@gmail.com>
2021-02-22 12:01:34 +0200

    Update

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.post('/register', (req, res) => {
         if (data.toString() !== ''){
             let searchUser = arrUsers.find(user => user.email === req.body.email);
             if(searchUser){
-                res.redirect('/error');
+                res.render('error', {existingEmail: 'Error! User with this nickname is already registered. Please choose another nickname or if this is your registered nickname, please log in to your account.'});
             }
             else{
                 arrUsers.push(req.body);
@@ -77,19 +77,22 @@ app.get('/login', (req, res) => {
 
 app.post('/login', (req, res) => {
     fs.readFile(path.join(__dirname, '/users/users.txt'), (err, data) => {
-        let users = JSON.parse(data.toString());
-        // let searchUserDb =  users.find(user => user.email === req.body.email && user.password === req.body.password);
-        let searchUserDb = users.findIndex(user => user.email === req.body.email && user.password === req.body.password);
-        let searchUserEmail = users.find(user => user.email === req.body.email);
-        if(searchUserDb !== -1){
-            res.redirect(`/user/${searchUserDb}`);
-            // res.send(searchUserDb);
-        } else {
-            if(!searchUserEmail){
-                res.send('There is no user with this email. Please register');
-            }else{
-                res.send('Invalid password');
+        if (data.toString() !== '') {
+            let users = JSON.parse(data.toString());
+            // let searchUserDb =  users.find(user => user.email === req.body.email && user.password === req.body.password);
+            let searchUserDb = users.findIndex(user => user.email === req.body.email && user.password === req.body.password);
+            let searchUserEmail = users.find(user => user.email === req.body.email);
+            if (searchUserDb !== -1) {
+                res.redirect(`/user/${searchUserDb}`);
+            } else {
+                if (!searchUserEmail) {
+                    res.render('error', {noUsers: 'There is no user with this email. Please register'});
+                } else {
+                    res.render('error', {invPassword: 'Invalid password'});
+                }
             }
+        }else{
+            res.render('error', {noUsers: 'There is no user with this email. Please register'});
         }
     });
 });
